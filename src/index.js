9bb1b3800a721cd22df3a19ad530b67af36b346a
Aibol Kussain <aibolikdev@gmail.com>
2017-12-02 22:39:02 +0300

    submit: submit task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getPositiveKeysOfBoolset(boolset) {
-  // your solution here
+  var result = boolset.filter(function(item, index) {
+    if (index % 2 === 1) return false
+    return boolset[index + 1] === true
+  });
+  return result;
 }
